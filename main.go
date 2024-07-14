2f0c932c021e8b33743d4ccaacd64ecf733e89ef
rafia9005 <rafia9005@gmail.com>
2024-07-14 10:55:08 +0700

    add: vercel.json

--- main.go
+++ main.go
@@ -3,23 +3,29 @@ package main
 import (
 	"Learn-Go-Api/database"
 	"Learn-Go-Api/routes"
+	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
-func main(){
-  app := fiber.New()
-  // load dotenv
-  godotenv.Load()
+func main() {
+	app := fiber.New()
+	// load dotenv
+	godotenv.Load()
 
-  //database connect
-  database.Connect()
+	// database connect
+	database.Connect()
 
-  //auto migration
-  routes.AutoMigrate()
-  routes.SetupRouter(app)
+	// auto migration
+	routes.AutoMigrate()
+	routes.SetupRouter(app)
+	port := os.Getenv("APP_PORT")
 
-  //port listen
-  app.Listen(":3000")
+	if port == "" {
+		app.Listen(":3000")
+	} else {
+		app.Listen(fmt.Sprintf(":%s", port))
+	}
 }
